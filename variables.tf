20683e97005d392021e0132cc73f0d5974eba8c0
ckckcklab <ckilpatrick82@gmail.com>
2021-09-07 14:46:07 +0100

    environment variable update

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "user_data" {
-  description = "The user data to provide when launching the instance."
+  description = "The user data to provide when launching the instance. Do not pass gzip-compressed data via this argument; see user_data_base64 instead."
   type        = string
   default     = <<EOF
 
@@ -15,3 +15,11 @@ EOF
 
 }
 
+variable "AWS_ACCESS_KEY_ID" {
+    type        = string
+    description = "AWS Access Key Environmental Variable."
+}
+variable "AWS_SECRET_ACCESS_KEY" {
+    type        = string
+    description = "AWS Secret Access Key Environmental Variable."
+}

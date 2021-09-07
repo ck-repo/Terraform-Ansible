cc7e4c4b3811b325eaf5bf93377ec1e7efcebfb0
ckckcklab <ckilpatrick82@gmail.com>
2021-09-07 10:17:57 +0100

    initial commit

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "user_data" {
-  description = "The user data to provide when launching the instance. Do not pass gzip-compressed data via this argument; see user_data_base64 instead."
+  description = "The user data to provide when launching the instance."
   type        = string
   default     = <<EOF
 
@@ -14,14 +14,3 @@ variable "user_data" {
 EOF
 
 }
-
-
-
-#!/bin/bash
-  #sudo su
-  #yum -y install httpd
-  #echo '<p> My Instance Deployed by Terraform! </p>' >> /var/www/html/index.html
-  #sudo systemctl enable httpd
-  #sudo systemctl start httpd
-
-  #wget https://kilp-ansible.s3.amazonaws.com/httpd.yaml

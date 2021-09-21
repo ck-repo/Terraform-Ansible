93efdcf4a4114301a2bf11005db62f2796fcd5bd
ckckcklab <ckilpatrick82@gmail.com>
2021-09-21 16:43:23 +0100

    update tf file to test tfsec

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "ec2_instance" {
   ami                    = var.ami
   instance_type          = var.instance_type
   key_name               = var.key_name
-  monitoring             = true
+  monitoring             = false
   vpc_security_group_ids = var.vpc_security_group_ids
   subnet_id              = var.subnet_id
   user_data              = var.user_data
